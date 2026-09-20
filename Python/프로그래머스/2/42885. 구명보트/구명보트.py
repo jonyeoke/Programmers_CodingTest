3edae77cb475936a94575c2dc0c0d2e8ae7896a5
JongHyeok LEE <144398689+jonyeoke@users.noreply.github.com>
2026-09-20 17:34:43 +0900

    [level 2] Title: 구명보트, Time: 7.88 ms, Memory: 12 MB -BaekjoonHub

---
+++ Python/프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,16 @@
+def solution(people, limit):
+    answer = 0
+    
+    boat = []
+    people=sorted(people)
+    left, right = 0, len(people)-1
+    while left <= right:
+        if people[left]+people[right]<=limit:
+            left+=1
+            right-=1
+            answer+=1
+        else:
+            right-=1
+            answer+=1
+    
+    return answer
